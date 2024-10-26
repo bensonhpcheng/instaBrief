44ef4f4c57bf81efe465dc614b8a5667b70bce69
Benson Cheng <bensonhpcheng@hotmail.com>
2024-10-26 16:20:43 -0400

    React test setup

--- instaBrief/src/Components/App.jsx
+++ instaBrief/src/Components/App.jsx
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+
+const App = () => {
+  console.log('App component rendered');
+  return (
+    <div>
+      <h1>Welcome to InstaBrief</h1>
+    </div>
+  );
+};
+
+export default App;
--- instaBrief/webpack.config.js
+++ instaBrief/webpack.config.js
@@ -1,46 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');  // Add this for HMR plugin
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.js', // Entry point of your app
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/',  // Important for HMR
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx)$/,  // For JS and JSX files
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-class-properties']
-          }
+          },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx'],  // Allows imports without specifying file extensions
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/index.html',  // Template for index.html
     }),
+    new webpack.HotModuleReplacementPlugin(),  // Enable HMR
   ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
-    hot: true,
-    port: 8080,
-    historyApiFallback: true,
+    compress: true,
+    hot: true,  // Enable Hot Module Replacement
+    port: 9000,
   },
 };
